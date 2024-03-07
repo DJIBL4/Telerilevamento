96ebacc0186e44a0a93e91fb84f99ac362434d90
albertotarroni <125457567+DJIBL4@users.noreply.github.com>
2024-03-07 17:30:24 +0100

    Create 02_Installing_Packages.R

---
+++ 02_Installing_Packages.R
@@ -0,0 +1,3 @@
+# Installing new packages in R
+install.packages("terra")  #uso le "" nella funzione racchiudendo il pacchetto, va di default nel CRAN, per quelli su github si usa un'altra funzione (CRAN, archivio online, un network per pacchetti di R), server nel mondo ai quali la funzione si rivolge.
+library(terra)    #funzione per richiamare i pacchetti installati su R, carica il pacchetto ed è possibile usarne le funzioni
